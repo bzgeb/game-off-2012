cff864b14f84f977d884385e75efbb36e24986dc
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-11-30 23:47:58 -0500

    Fixed the bug where you could mash B to win

--- Assets/Scripts/RhythmTracker.cs
+++ Assets/Scripts/RhythmTracker.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RhythmTracker : MonoBehaviour {
 
@@ -14,12 +15,18 @@ public class RhythmTracker : MonoBehaviour {
 	private const bool USE_FRAME_COUNT = true;
 	private int beats_queued;
 	private int inputs_queued;
+	private Queue<float> queued_ticks;
+	private Queue<float> queued_input;
+	private bool dropped_tick;
 	
 	void Start () 
 	{
 		last_tick_time = float.MaxValue;
 		last_press_time = float.MinValue;
 		player = FindObjectOfType(typeof(Player)) as Player;
+		
+		queued_input = new Queue<float>();
+		queued_ticks = new Queue<float>();
 	}
 	
 	// Update is called once per frame
@@ -42,64 +49,80 @@ public class RhythmTracker : MonoBehaviour {
 			}
 		}
 #else
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			Debug.Break();
+		}
 		if (Input.GetKeyUp(KeyCode.B))
 		{
 			GotInput();
 		}
 #endif	
-		if (got_input)
-		{
-			float diff;
-			if (USE_FRAME_COUNT)
-			{
-				diff = Mathf.Abs(last_tick_frame - last_press_frame);
-				if (diff < 14)
-				{
-					streak += 3;
-					print("Great!");
-				}
-				else if (diff < 20)
-				{
-					streak += 1;
-					print("Okay!");
-				}
-				else
-				{
-					streak = Mathf.Clamp(streak - 1, 0, streak);
-					print("Bad!");				
-				}
-			}
-			else
-			{
-				diff = Mathf.Abs(last_tick_time - last_press_time);
-				if (diff < 0.1f)
-				{
-					streak += 3;
-					print("Great!");
-				}
-				else if (diff < 0.4f)
-				{
-					streak += 1;
-					print("Okay!");
-				}
-				else
-				{
-					streak = Mathf.Clamp(streak - 1, 0, streak);
-					print("Bad!");
-				}
-			}
-			print("Diff: " + diff);
-			
-			got_input = false;
-		}
+//		if (got_input)
+//		{
+//			float diff;
+//			if (USE_FRAME_COUNT)
+//			{
+//				diff = Mathf.Abs(last_tick_frame - last_press_frame);
+//				if (diff < 14)
+//				{
+//					streak += 3;
+//					print("Great!");
+//				}
+//				else if (diff < 20)
+//				{
+//					streak += 1;
+//					print("Okay!");
+//				}
+//				else
+//				{
+//					streak = Mathf.Clamp(streak - 1, 0, streak);
+//					print("Bad!");				
+//				}
+//			}
+//			else
+//			{
+//				diff = Mathf.Abs(last_tick_time - last_press_time);
+//				if (diff < 0.1f)
+//				{
+//					streak += 3;
+//					print("Great!");
+//				}
+//				else if (diff < 0.4f)
+//				{
+//					streak += 1;
+//					print("Okay!");
+//				}
+//				else
+//				{
+//					streak = Mathf.Clamp(streak - 1, 0, streak);
+//					print("Bad!");
+//				}
+//			}
+//			print("Diff: " + diff);
+//			
+//			got_input = false;
+//		}
 	}
 	
 	void GotInput()
 	{
 		last_press_time = Time.realtimeSinceStartup;
 		last_press_frame = Time.frameCount;
+		
 		CancelInvoke("fail");
-		got_input = true;
+		
+		if (queued_ticks.Count > 0)
+		{
+			CheckTiming(last_press_frame, queued_ticks.Dequeue());
+			queued_ticks.Clear();
+		}
+		else if(!dropped_tick)
+		{
+			queued_input.Enqueue(last_press_frame);
+		}
+		
+//		got_input = true;
 	}
 	
 //	void OnGUI()
@@ -130,7 +153,46 @@ public class RhythmTracker : MonoBehaviour {
 	{
 		last_tick_time = Time.realtimeSinceStartup;
 		last_tick_frame = Time.frameCount;
-
-		Invoke("fail", 60 / bpm);
+		
+		if (dropped_tick)
+		{
+			dropped_tick = false;
+		}
+		
+		if (queued_input.Count > 1)
+		{
+			fail();
+			queued_input.Clear();
+			dropped_tick = true;
+		}
+		else if (queued_input.Count == 1)
+		{
+			CheckTiming(queued_input.Dequeue(), last_tick_frame);
+		}
+		else
+		{
+			queued_ticks.Enqueue(last_tick_frame);
+			Invoke("fail", 60 / bpm);
+		}
+	}
+	
+	void CheckTiming(float input_time, float beat_time)
+	{
+		float diff = Mathf.Abs(beat_time - input_time);
+		if (diff < 14)
+		{
+			streak += 3;
+			print("Great!");
+		}
+		else if (diff < 20)
+		{
+			streak += 1;
+			print("Okay!");
+		}
+		else
+		{
+			streak = Mathf.Clamp(streak - 1, 0, streak);
+			print("Bad!");				
+		}
 	}
 }
